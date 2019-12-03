5c709ff15415c3234e7d6539fa419d840b799d0e
David <d.ikkes2010@gmail.com>
2019-12-03 23:31:31 +0200

    added User model for db

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db = SQLAlchemy(app=app)
+migrate = Migrate(app=app, db=db)
 
 
 from src import routes
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from flask import send_from_directory
 
 from app import app
@@ -37,7 +37,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f"Login request for user -> {form.username.data}, remember_me={form.remember_me.data}")
-        return redirect("/index")
+        return redirect(url_for('index'))
     return render_template("login.html",
                            title="Sign In",
                            form=form)
